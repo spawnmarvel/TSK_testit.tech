a2571ed8ae8443d737531f95dd438c2f000180ea
spawnmarvel <espenkleivane@gmail.com>
2017-11-21 22:50:46 +0100

    pylint check for sqlalchemy_statment

--- app/mod_db/sqlalchemy_statments.py
+++ app/mod_db/sqlalchemy_statments.py
@@ -1,102 +1,109 @@
-from app.mod_db import sqlalchemy_declarative
-
+"""___"""
 import sqlite3
 import datetime
-
-conn = None
-database_ = "app/mod_db/database.db"
-sql_select_all = "select * from holder order by id desc"
-sql_select_max_id = "select max(id) from holder"
-sql_insert = "insert into holder (id, note, topic, url, published) values (?, ?, ?, ?, ?)"
-sql_delete = "Delete from holder where id = ?"
-
 import logging
-logger = logging.getLogger(__name__)
+from app.mod_db import sqlalchemy_declarative
+
+
+
+CONN_ = None
+DATABASE_ = "app/mod_db/DATABASE.db"
+SQL_SELECT_ALL = "select * from holder order by id desc"
+SQL_SELECT_MAX_ID = "select max(id) from holder"
+SQL_INSERT = "insert into holder (id, note, topic, url, published) values (?, ?, ?, ?, ?)"
+SQL_DELETE = "Delete from holder where id = ?"
+
+LOGGER_ = logging.getLogger(__name__)
 
 def get_database():
-    global database_
-    return database_
+    """___"""
+    global DATABASE_
+    return DATABASE_
 
 def get_all():
+    """___"""
     msg = ""
-    global conn
+    global CONN_
     try:
-        conn = sqlite3.connect(get_database())
-        with conn:
-            cur = conn.cursor()
-            global sql_select_all
-            cur.execute(sql_select_all)
+        CONN_ = sqlite3.connect(get_database())
+        with CONN_:
+            cur = CONN_.cursor()
+            global SQL_SELECT_ALL
+            cur.execute(SQL_SELECT_ALL)
             row = cur.fetchall()
             msg = row
-            logger.debug(msg)
-    except sqlite3.OperationalError as e:
-        msg = str(e)
-        logger.debug(msg)
+            LOGGER_.debug(msg)
+    except sqlite3.OperationalError as error_:
+        msg = str(error_)
+        LOGGER_.debug(msg)
     return msg
 
 def get_max_id():
+    """___"""
     msg = ""
-    global conn
+    global CONN_
     try:
-        conn = sqlite3.connect(get_database())
-        with conn:
-            cur = conn.cursor()
-            global sql_select_all
-            cur.execute(sql_select_max_id)
+        CONN_ = sqlite3.connect(get_database())
+        with CONN_:
+            cur = CONN_.cursor()
+            global SQL_SELECT_ALL
+            cur.execute(SQL_SELECT_MAX_ID)
             row = cur.fetchone()
             msg = row
-    except sqlite3.OperationalError as e:
-        msg = str(e)
-    logger.debug(msg)
+    except sqlite3.OperationalError as error_:
+        msg = str(error_)
+    LOGGER_.debug(msg)
     return msg
 
 
 def insert(note, topic, url_to_save):
+    """___"""
     msg = None
-    global conn
+    global CONN_
     try:
-        conn = conn = sqlite3.connect(get_database())
-        with conn:
-            cur = conn.cursor()
+        CONN_ = sqlite3.connect(get_database())
+        with CONN_:
+            cur = CONN_.cursor()
             time_now = datetime.datetime.now()
-            global sql_insert
+            global SQL_INSERT
             # get max id and increase it with 1
             tmp_id = get_max_id()
             max_id = tmp_id[0] + 1
-            cur.execute(sql_insert, (max_id,note,topic,url_to_save, time_now))
-            conn.commit()
+            cur.execute(SQL_INSERT, (max_id, note, topic, url_to_save, time_now))
+            CONN_.commit()
             msg = "Added 1 row to sqlite3, with id " + str(max_id)
-            
-    except sqlite3.OperationalError as e:
-        msg = e
-        conn.rollback()
-    except sqlite3.IntegrityError as e:
-        msg = e
-        conn.rollback()
-    except sqlite3.InterfaceError as e:
-        msg = e
-        conn.rollback()
-    logger.debug(msg)
+
+    except sqlite3.OperationalError as error_:
+        msg = error_
+        CONN_.rollback()
+    except sqlite3.IntegrityError as error_:
+        msg = error_
+        CONN_.rollback()
+    except sqlite3.InterfaceError as error_:
+        msg = error_
+        CONN_.rollback()
+    LOGGER_.debug(msg)
     return msg
 
 def delete(id_):
+    """___"""
     msg = None
-    global conn
+    global CONN_
     try:
-        conn = conn = sqlite3.connect(get_database())
-        with conn:
-            cur = conn.cursor()
-            global sql_delete
-            cur.execute(sql_delete, (id_,))
-            conn.commit()
+        CONN_ = sqlite3.connect(get_database())
+        with CONN_:
+            cur = CONN_.cursor()
+            global SQL_DELETE
+            cur.execute(SQL_DELETE, (id_,))
+            CONN_.commit()
             msg = "Delete 1 row, id " + str(id_) + "."
-    except sqlite3.OperationalError as e:
-        msg = e
-        conn.rollback()
-    except sqlite3.IntegrityError as e:
-        msg = e
-        conn.rollback()
-    logger.debug(msg)
+    except sqlite3.OperationalError as error_:
+        msg = error_
+        CONN_.rollback()
+    except sqlite3.IntegrityError as error_:
+        msg = error_
+        CONN_.rollback()
+    LOGGER_.debug(msg)
     return msg
 
 sqlalchemy_declarative.init()
