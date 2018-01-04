a195318326ea890180ee878444950ed1535d02e7
spawnmarvel <espenkleivane@gmail.com>
2018-01-04 22:35:41 +0100

    added built in functions html / views

--- test/test.py
+++ test/test.py
@@ -6,16 +6,38 @@ __func__ = "test_enumerator()"
 
 
 
-li = [1, 2, 8, 79]
 
 
-def test_enumerator(li):
-    
-    for i, item in enumerate(li):
+
+
+
+
+NUM_LIST = [1, 2, 8, 79]
+rv = "Length of num list: {} ".format(len(NUM_LIST))
+# print(rv)
+
+def test_enumerator(numbr_list):
+    """Test enumerate"""
+    for i, item in enumerate(numbr_list):
         print("index " + str(i) + " item " + str(item))
 
-# test_enumerator(li)
+def test_range():
+    """Test range"""
+    for item_x in range(6):
+        print(item_x)
 
+def test_zip():
+    """test zip"""
+    x = [1, 2, 3]
+    y = [4, 5, 6]
+    zipped = zip(x, y)
+    for item_x in zipped:
+        print(item_x)
 
-
-   
+y = "128"
+print(type(y))
+x = int(y)
+print(type(x))
+# test_zip()
+# test_range()
+# test_enumerator(NUM_LIST)
